4f87e51982001b7d4e20f2b83e618ce79315b31d
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2025-03-01 17:11:56 +0200

    feat: started my portoflio site.

---
+++ protfolio_site/script.js
@@ -0,0 +1,29 @@
+const texts = ["I'm Rosen Ivanov!", 'I am 16 years old!',  'I am learning to be full stack engineer!', 'Hobbies: Tennis, School, Playing games!'];
+const speed = 100;
+const delay = 1000;
+let index = 0;
+let textIndex = 0;
+
+function typeWriter() {
+    if (index < texts[textIndex].length) {
+        document.getElementById("typing").innerHTML += texts[textIndex].charAt(index);
+        index++;
+        setTimeout(typeWriter, speed);
+    } else {
+        setTimeout(deleteText, delay);
+    }
+}
+
+function deleteText() {
+    const currentText = document.getElementById("typing").innerHTML;
+    if (currentText.length > 0) {
+        document.getElementById("typing").innerHTML = currentText.substring(0, currentText.length - 1);
+        setTimeout(deleteText, speed);
+    } else {
+        index = 0;
+        textIndex = (textIndex + 1) % texts.length;
+        setTimeout(typeWriter, delay);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", typeWriter);
