693981c61ddc8068e4da2979caa80a34ce121b6d
MacBookPro16 <rrirrirri08@gmail.com>
2025-04-22 18:44:49 +0300

    feat: upgrated when clicking the download cv btn.

---
+++ cv.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let cv = document.querySelector('.a-wrap > a');
+
+    cv.addEventListener('click', handleClickingCv);
+
+    function handleClickingCv() {
+        alert("Still dont't have, but coming soon.....")
+    }
+})
